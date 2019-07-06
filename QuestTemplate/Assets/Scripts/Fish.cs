d4507c4d9916ae04c6b79de1fd13c00add7f0a70
Daniel <dabellian@gmail.com>
2019-07-06 18:53:55 -0500

    imported boaryard asset

--- QuestTemplate/Assets/Scripts/Fish.cs
+++ QuestTemplate/Assets/Scripts/Fish.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     Animator animator;
-    
+    Rigidbody rigidbod;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {  
+        rigidbod = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.y>0.215)
-            animator.SetBool("Flop", true);
-        else
+        if (gameObject.transform.position.y>0)
         {
+            rigidbod.useGravity = true;
+            animator.SetBool("Flop", true);
+        }
+        
+        else if (rigidbod.useGravity == true)
+        {
+            rigidbod.useGravity = false;
+            rigidbod.velocity = new Vector3(0, 0, 0);
             animator.SetBool("Flop", false);
         }
     }
---
+++ QuestTemplate/Assets/Scripts/TriggerAnimate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TriggerAnimate : MonoBehaviour
+{
+    public bool persistAfterDeparture;
+    public bool invertActivation;
+    public string animgraphBoolName;
+    private Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.transform.root.tag == "Player")
+        {
+            
+                if (invertActivation==false)
+                animator.SetBool(animgraphBoolName, true);
+                if (invertActivation==true)
+                animator.SetBool(animgraphBoolName, false);
+        }
+    }
+    private void OnTriggerExit(Collider Other)
+    {
+        if (Other.gameObject.transform.root.tag == "Player")
+        {
+            if (persistAfterDeparture==false)
+
+                if (invertActivation==false)
+                    animator.SetBool(animgraphBoolName, false);
+                if (invertActivation==true)
+                    animator.SetBool(animgraphBoolName, true);
+        }
+    }
+}
