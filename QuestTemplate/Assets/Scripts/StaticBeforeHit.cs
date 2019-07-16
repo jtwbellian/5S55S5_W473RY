0c94977cfc227161b888bd8e4baf7c65a9cd86bb
Daniel <dabellian@gmail.com>
2019-07-15 19:49:04 -0500

    food barrel, apples, clam updates

---
+++ QuestTemplate/Assets/Scripts/StaticBeforeHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticBeforeHit : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rigidbod;
+    // Start is called before the first frame update
+    void Start()
+    {
+    rigidbod = GetComponent<Rigidbody>();
+    animator = GetComponent<Animator>();
+    rigidbod.useGravity=false;
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (rigidbod.useGravity==false)
+            rigidbod.useGravity=true;
+    }
+}
