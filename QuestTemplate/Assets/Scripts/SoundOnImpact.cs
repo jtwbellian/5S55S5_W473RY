2556ef0c8b88724a6bb9f485a9840e741ba1d2e5
binaridan <dabellian@gmail.com>
2019-08-12 19:35:30 -0500

    soundimpactscript

---
+++ QuestTemplate/Assets/Scripts/SoundOnImpact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnImpact : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private SoundManager sm;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    sm = SoundManager.instance;
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision c)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        sm.PlaySingle(clips[randomIndex], transform.position);
+    }
+}
