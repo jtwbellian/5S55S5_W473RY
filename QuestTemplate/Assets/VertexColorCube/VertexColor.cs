b5b8b7f6c062f9b14b3e4f127ac4bcbe1ee85747
Daniel Bellian <dabellian@gmail.com>
2020-01-31 15:32:40 -0600

    who the fuck knows

---
+++ QuestTemplate/Assets/VertexColorCube/VertexColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexColor : MonoBehaviour
+{
+    [SerializeField] [ReadOnly]
+    private string note = "Color only visible at runtime";
+
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ColorAllVerts();
+    }
+
+    void ColorAllVerts()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+
+        List<Color32> vertexColorList= new List<Color32>();
+            for (int i = 0; i < mesh.vertexCount; i++)
+            {
+                vertexColorList.Add(color);
+            }
+        mesh.SetColors(vertexColorList);
+    }
+}
